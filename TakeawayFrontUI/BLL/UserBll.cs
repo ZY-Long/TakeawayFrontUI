3734a7d7c3340a5c90d4e07c7b2c91f28f96eb45
彦隆 张 <3086362103@qq.com>
2020-03-23 14:09:33 +0800

    搭建框架

---
+++ TakeawayFrontUI/BLL/UserBll.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SDK;
+
+namespace BLL
+{
+    public class UserBll
+    {
+
+        /// <summary>
+        /// 添加用户
+        /// </summary>
+        public UserAddResponse AddUser(UserAddRequest userAddRequest)
+        {
+            return ApiRequestHelper.Post<UserAddRequest, UserAddResponse>(userAddRequest);
+        }
+
+        /// <summary>
+        /// 获取用户
+        /// </summary>
+        /// <param name="request"></param>
+        public void GetUserInfo(UserGetRequest request)
+        {
+            UserGetResponse response = new UserGetResponse();
+            ApiRequestHelper.Post<UserGetRequest, UserGetResponse>(request);
+        }
+    }
+}
---
+++ TakeawayFrontUI/TakeawayFrontUI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using SDK;
+
+namespace TakeawayFrontUI.Controllers
+{
+    public class UserController : Controller
+    {
+        UserBll userBll = new UserBll();
+        // 注册页面
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        ///// <summary>
+        ///// 用户注册
+        ///// </summary>
+        //public JsonResult UerRegist(UserAddRequest userAddRequest)
+        //{
+        //    //get请求
+        //    //return Json(userBll.AddUser(userAddRequest), JsonRequestBehavior.AllowGet);
+
+        //    return Json(userBll.AddUser(userAddRequest));
+        //}
+
+    }
+}
