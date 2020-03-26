cb301d79c12a13d6abea749f70e6fcbed7914d0c
TR <TR@DESKTOP-3LFUUQC>
2020-03-26 15:45:06 +0800

    ...

---
+++ TakeawayFrontUI/TakeawayFrontUI/Controllers/ShowCartInfoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL;
+using SDK;
+
+namespace TakeawayFrontUI.Controllers
+{
+    public class ShowCartInfoController : Controller
+    {
+        UserBll bll = new UserBll();
+        // GET: ShowCartInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetTake(TakeRequest request)
+        {
+            return Json(bll.GetTake(request));
+        }
+        /// <summary>
+        /// 显示购物车
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public JsonResult GetCart(GetCartRequest request)
+        {
+            return Json(bll.GetCart(request), JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 删除菜品
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public JsonResult DeleteCart(DeleteCartRequest request)
+        {
+            return Json(bll.DeleteCart(request));
+        }
+    }
+}
